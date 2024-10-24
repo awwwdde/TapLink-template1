60951c5f30077ba040f873b12f1b504f2b968d03
awwwdde <sjuu1337@gmail.com>
2024-10-24 23:22:46 +0300

    Update #2 added styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 
-
+import Main from './pages/main/Main';
+import './styles/styles.scss';
 function App() {
   return (
     <div className="App">
-      hello
+      <Main/>
     </div>
   );
 }
---
+++ src/pages/main/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import style from './main.module.scss'
+function Main() {
+  return (
+    <div className={style.main}>
+      hello
+    </div>
+  )
+}
+
+export default Main
