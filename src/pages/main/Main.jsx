23a1583b946308a5f8703b5c3ea93e73b6826977
awwwdde <sjuu1337@gmail.com>
2024-10-26 02:34:17 +0300

    Update #7

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -6,12 +6,12 @@ function Main() {
   return (
     <div className="main">
         <div className="main-block">
-            <div className="main-block__title">TAPLINK. Template</div>
+            <div className="main-block__title">TAPLINK. Prototype</div>
             <div className="main-block__img"></div>
             <div className="main-block__subtitle">Suspendisse quis tincidunt lacus. In hendrerit tristique elit, vitae rhoncus nulla iaculis ac. Pellentesque sodales justo quis sapien tristique feugiat. Integer placerat diam quis tellus vestibulum, ut ultricies mi finibus. Ut congue, lectus vel aliquam pulvinar, neque orci ultrices augue, vel viverra dui arcu id velit. In maximus non felis quis dictum. Nunc convallis vehicula massa scelerisque finibus. Etiam et sollicitudin nunc, sit amet consequat nulla.</div>
             <div className="main-block__links">
-                <button className="main-block__links-btn">Portfolio</button>
-                <button className="main-block__links-btn">Contact</button>
+                <div className="main-block__links-btn">Portfolio</div>
+                <div className="main-block__links-btn">Contact</div>
             </div>
         </div>
         <Footer/>
